6a2ce9a13e17572af03d63f1bcc5876429eb52f8
Ashish Patil <ashishnitinpatil@gmail.com>
2022-08-06 22:39:19 +0400

    ++completed task 4 / upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,21 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+username=ashishnitinpatil
+dockerpath="$username/udacity_nd9991_project_4"
+echo "$username $dockerpath"
 
 # Step 2:  
 # Authenticate & tag
+docker login -u $username
 echo "Docker ID and Image: $dockerpath"
+echo "Please enter current version tag"
+read version
+echo "Tagging image as 'latest' and '$version'"
+docker image tag nd991_project_4 $dockerpath:latest
+docker image tag nd991_project_4 $dockerpath:$version
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
+docker push $dockerpath:$version
